a4a2c4b88737d4d32a8b6832b9dafa653cddd102
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-10-17 22:40:13 -0300

    Criando projetos de testes e adicionando referência de código

---
+++ TDD-DIO.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using TDD_DIO;
+using Xunit;
+
+namespace TDD_DIO.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Calculadora calc = new Calculadora();
+    }
+}
